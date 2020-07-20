8619607413d24df92d15b2e8929eadebe864a711
theducvu <vutheduc96@gmail.com>
2020-07-20 12:26:48 +1000

    Navbar + add games

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,16 +1,1 @@
 
-// TODO: Add SDKs for Firebase products that you want to use
-//  https://firebase.google.com/docs/web/setup#available-libraries -->
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyDEzAIQwfLg29TkWaXMxf7RQOSQmnKpcnI",
-    authDomain: "utsplaymakerswebsite.firebaseapp.com",
-    databaseURL: "https://utsplaymakerswebsite.firebaseio.com",
-    projectId: "utsplaymakerswebsite",
-    storageBucket: "utsplaymakerswebsite.appspot.com",
-    messagingSenderId: "1089423557436",
-    appId: "1:1089423557436:web:f86ef9ff928c4eaf929439"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
