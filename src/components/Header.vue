a1b4fcdc59cd5f96b59f5078b0c0d0193c754fdd
AndrewVonStieglitz <vonstieglitzandrew@gmail.com>
2022-02-13 11:48:33 +1100

    Website

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <scroll-fixed-header :fixed.sync="fixed" :threshold="56">
+      <div class="shane_tm_topbar">
+        <div class="topbar_inner">
+          <div class="logo">
+            <router-link to="/">
+              <img src="../assets/img/logo/new/dark.png" alt="partners brand" />
+            </router-link>
+          </div>
+          <!-- End logo -->
+          <div class="menu">
+            <scrollactive
+              class="anchor_nav"
+              bezier-easing-value=".5,0,.35,1"
+              :offset="95"
+              :duration="100"
+              :alwaysTrack="true"
+            >
+              <a href="#home" class="scrollactive-item">Home</a>
+              <a href="#about" class="scrollactive-item">About</a>
+              <a href="#portfolio" class="scrollactive-item">Portfolio</a>
+              <a href="#news" class="scrollactive-item">News</a>
+
+              <a
+                class="white-fill-bg btn-outline btn_sm"
+                target="_blank"
+                rel="noreferrer"
+                href="#"
+              >
+                Buy Now
+              </a>
+            </scrollactive>
+          </div>
+          <!-- End menu -->
+        </div>
+      </div>
+      <!--  End shane_tm_topbar -->
+    </scroll-fixed-header>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      active: false,
+      fixed: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/all-home-version/CreativePortfolioTwo.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home-four">
+
+    <Slider />
+    <!-- End Slider Section -->
+
+
+    <div class="shane_tm_section" id="portfolio">
+      <div class="shane_tm_portfolio">
+        <Portfolio />
+      </div>
+    </div>
+    <!-- End Portfolio Section -->
+
+    <div class="shane_tm_section" id="news">
+      <div class="shane_tm_news">
+        <div class="container">
+          <div class="shane_tm_title">
+            <span>News</span>
+            <h3>Latest News</h3>
+          </div>
+          <!-- End shane_tm_title -->
+          <News />
+          <!-- End news  -->
+        </div>
+      </div>
+    </div>
+    <!-- End  news -->
+
+
+    <!-- End  call to actions -->
+
+    <Footer />
+    <!-- End  footer  -->
+  </div>
+</template>
+
+<script>
+import Slider from "../../components/slider/SliderFour";
+
+import Portfolio from "../../components/portfolio/PortfolioTwo";
+
+
+
+import News from "../../components/news/NewsTwo";
+
+import Footer from "../../components/Footer";
+export default {
+  components: {
+    Slider,
+
+    Portfolio,
+
+
+    News,
+
+    Footer,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
